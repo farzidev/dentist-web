2383ca9ae2220933d9d8d768566b6cf48030f10c
Aman Rawat <rawataman340@gmail.com>
2020-02-21 12:18:35 +0530

    corrected-collapsable-menu-bug

--- src/components/ListedDoctors/CollapsableBooking/CollapsableBooking.js
+++ src/components/ListedDoctors/CollapsableBooking/CollapsableBooking.js
@@ -6,19 +6,19 @@ import "./CollapsableBooking.css";
 export default function CollapsableBooking() {
   const [open, setOpen] = useState(false);
 
-  const removeButton = () => {
-    document.getElementById("book-now-card-btn-id").style.display = "none";
-  };
+  // const removeButton = () => {
+  //   document.getElementById("book-now-card-btn-id").style.display = "none";
+  // };
 
   return (
     <React.Fragment>
       <div className="book-now-card-btn-container" id="book-now-card-btn-id">
         <Button
-          className="test-button"
+          className={!open ? "test-button" : "test-button-hide"}
           size="md"
           onClick={() => {
             setOpen(!open);
-            removeButton();
+            // removeButton();
           }}
           aria-expanded={open}
         >
